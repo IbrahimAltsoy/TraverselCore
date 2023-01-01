95bc2b1e4192856d3a20e14ddc17459bb2801c04
IbrahimAltsoy <78874266+IbrahimAltsoy@users.noreply.github.com>
2023-01-01 14:26:57 +0300

    01.01.2023

---
+++ TraverselCore/Controllers/DestinationController.cs
@@ -0,0 +1,32 @@
+﻿using BusiinessLayer.Abstract;
+using EntityLayer.Concreate;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TraverselCore.Controllers
+{
+    public class DestinationController : Controller
+    {
+        private readonly IService<Destination> _service;
+
+        public DestinationController(IService<Destination> service)
+        {
+            this._service = service;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var model =await _service.GetAllAsync();
+            return View(model);
+        }
+        [HttpGet]
+        public IActionResult Details(int id)
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Details(Destination destination)
+        {
+            return View();
+        }
+    }
+}
