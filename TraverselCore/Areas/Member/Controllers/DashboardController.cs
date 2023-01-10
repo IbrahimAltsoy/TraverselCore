0a46736b5c32350c4f7e2975732ac92cbece9810
IbrahimAltsoy <78874266+IbrahimAltsoy@users.noreply.github.com>
2023-01-11 00:18:09 +0300

    11.01.2023

---
+++ TraverselCore/Areas/Member/Controllers/DashboardController.cs
@@ -0,0 +1,25 @@
+﻿using EntityLayer.Concreate;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TraverselCore.Areas.Member.Controllers
+{
+    [Area(nameof(Member))]
+    public class DashboardController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+
+        public DashboardController(UserManager<AppUser> userManager)
+        {
+            this._userManager = userManager;
+        }
+
+        public async Task<IActionResult> IndexAsync()
+        {
+            var model = await _userManager.FindByNameAsync(User.Identity.Name);
+            ViewBag.userName = model.Name + " " + model.Surname;
+            ViewBag.userImage = model.ImageUrl;
+            return View();
+        }
+    }
+}
