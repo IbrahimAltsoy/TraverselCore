7fc23fa4a3ae0fef877a68d53e82d2c67135b768
IbrahimAltsoy <78874266+IbrahimAltsoy@users.noreply.github.com>
2023-01-20 01:49:22 +0300

    .

---
+++ TraverselCore/Areas/Admin/Controllers/GuideController.cs
@@ -0,0 +1,37 @@
+﻿using BusiinessLayer.Abstract;
+using EntityLayer.Concreate;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TraverselCore.Areas.Admin.Controllers
+{
+    [Area(nameof(Admin))]
+    public class GuideController : Controller
+    {
+        private readonly IService<Guide> _guideService;
+
+        public GuideController(IService<Guide> guideService)
+        {
+            _guideService = guideService;
+        }
+
+        public async Task<IActionResult> IndexAsync()
+        {
+            var model = await _guideService.GetAllAsync();
+            return View(model);
+        }
+        [HttpGet]
+        public async Task<IActionResult> AddGuide()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> AddGuide(Guide guide)
+        {
+            await _guideService.AddAsync(guide);
+            await _guideService.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+
+    }
+}
