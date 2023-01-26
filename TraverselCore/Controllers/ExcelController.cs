d5e086a9d6341c4de04f9e3b965703a39418739b
IbrahimAltsoy <78874266+IbrahimAltsoy@users.noreply.github.com>
2023-01-27 01:22:38 +0300

    added Excel report

---
+++ TraverselCore/Controllers/ExcelController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OfficeOpenXml;
+
+namespace TraverselCore.Controllers
+{
+    public class ExcelController : Controller
+    {
+        public IActionResult Index()
+        {
+            ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+            
+            ExcelPackage package = new ExcelPackage();
+            var workShet = package.Workbook.Worksheets.Add("Sayfa_");
+            workShet.Cells[1, 1].Value = "Rota";
+            workShet.Cells[1, 2].Value = "Rehber";
+            workShet.Cells[1, 3].Value = "Kontenjan";
+            workShet.Cells[2, 1].Value = "Güneydoğu Turu";
+            workShet.Cells[2, 2].Value = "Selami Toparslan";
+            workShet.Cells[2, 3].Value = "27";
+            workShet.Cells[3, 1].Value = "Karadeniz Turu";
+            workShet.Cells[3, 2].Value = "Faruk Bayat";
+            workShet.Cells[3, 3].Value = "60";
+
+            var bytee = package.GetAsByteArray();
+
+            return File(bytee, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", "YeniListe.xlsx");
+        }
+    }
+}
