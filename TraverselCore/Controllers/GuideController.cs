811ebf7bee460ef4dbcde02f72f696d980a0f5c5
IbrahimAltsoy <78874266+IbrahimAltsoy@users.noreply.github.com>
2023-03-05 13:52:29 +0300

    added GuideController

---
+++ TraverselCore/Controllers/GuideController.cs
@@ -0,0 +1,25 @@
+﻿using BusiinessLayer.Abstract;
+using EntityLayer.Concreate;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TraverselCore.Controllers
+{
+    [AllowAnonymous]
+    public class GuideController : Controller
+    {
+        private readonly IService<Guide> _service;
+
+        public GuideController(IService<Guide> service)
+        {
+            _service = service;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var model =await _service.GetAllAsync(x=>x.Statu==false);
+            int a = 5;
+            return View(model);
+        }
+    }
+}
