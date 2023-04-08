2cc4b84de1688cd2c22490a0b573c9117154e9ac
IbrahimAltsoy <78874266+IbrahimAltsoy@users.noreply.github.com>
2023-04-08 13:36:29 +0300

    ..

--- TraverselCore/ViewComponents/MemberDashboard/_CheapDestination.cs
+++ TraverselCore/ViewComponents/MemberDashboard/_CheapDestination.cs
@@ -4,17 +4,17 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace TraverselCore.ViewComponents.MemberDashboard
 {
-    public class _GuideList:ViewComponent
+    public class _CheapDestination:ViewComponent
     {
-        private readonly IService<Guide> _service1;
-        public _GuideList(IService<Guide> service1)
+        private readonly IService<Destination> _service1;
+        public _CheapDestination(IService<Destination> service1)
         {
             _service1 = service1;
         }
         public async Task<IViewComponentResult> InvokeAsync()
         {
             var model = await _service1.GetAllAsync();
-            return View(model.Take(5).ToList());
+            return View(model.Take(4).OrderByDescending(x=>x.Price).ToList());
         }
     }
 }
